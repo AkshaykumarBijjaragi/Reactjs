1776a846b6273e9acdf4e537c06de3cdd508ce08
unknown <akshaykumarbijjaragi@gmail.com>
2024-01-28 15:49:09 +0530

    Starting star rating feature

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Accordion from "./src/components/Accordion/Accordion";
-import Colorgenerator from "./src/components/Random color generator/RandomColorGenerator";
+// import Colorgenerator from "./src/components/Random color generator/RandomColorGenerator";
+import StarRatings from "./src/components/StarRating/StarRatings"
 
 const hello = <h1>hello from react</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Colorgenerator />);
+root.render(<StarRatings />);
---
+++ src/components/StarRating/StarRatings.jsx
@@ -0,0 +1,5 @@
+import "./index.css";
+
+export default function StarRatings() {
+  return <div className="star-rating">star rating</div>;
+}
